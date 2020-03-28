d5519267670e059897d04d9e0f47ad960f9378c7
Nguyễn Văn Triều <trieunv.it@gmail.com>
2020-03-28 08:31:25 +0700

    upload code

---
+++ app/src/main/java/hcmute/edu/vn/calculator_22/MainActivity.java
@@ -0,0 +1,16 @@
+package hcmute.edu.vn.calculator_22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
