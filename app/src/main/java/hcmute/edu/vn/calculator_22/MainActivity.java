6cd3bffdec5ee45292826c164c9a0aa2ef473fb4
Tam <tamst09@gmail.com>
2020-04-10 11:28:03 +0700

    edit event by Tam

--- app/src/main/java/hcmute/edu/vn/calculator_22/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/calculator_22/MainActivity.java
@@ -9,14 +9,43 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
     Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnDevide, btnMulti, btnSub, btnSum, btnDot, btnDel, btnResult;
-    String number1="";
     TextView txtResult;
-
+    int result;                         //kết quả cuối cùng
+    int temp;                               //biến lưu các giá trị tạm thời, khi nào người dùng click vào
+                                            // sự kiện tính toán thì đem ra tính toán với result
+    String pheptinh;                    //lưu trữ phép tính dưới dạng chuỗi "cong" "tru" "nhan" "chia"
+    boolean isCalculating;                  //true : khi đang trong một chuỗi tính toán và vẫn có thể tiếp tục tính toán với kết quả vừa có.
+                                            //false: khi đã hoàn thành chuỗi tính toán, như là click vào dấu = hay clear
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AnhXa();
+        result=0;
+        temp =0;
+        isCalculating=false;
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btn0.setOnClickListener(this);
+        btnDel.setOnClickListener(this);
+        btnDot.setOnClickListener(this);
+        btnDevide.setOnClickListener(this);
+        btnMulti.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnSum.setOnClickListener(this);
+        btnResult.setOnClickListener(this);
+        txtResult.setText(String.valueOf(result));          //ban đầu result mặc định = 0
+    }
 
+    protected void AnhXa()      //hàm ánh xạ các view
+    {
         btn1 = (Button) findViewById(R.id.btnNum1);
         btn2 = (Button) findViewById(R.id.btnNum2);
         btn3 = (Button) findViewById(R.id.btnNum3);
@@ -35,26 +64,7 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         btnSum = (Button) findViewById(R.id.btnSum);
         btnResult = (Button) findViewById(R.id.btnResult);
         txtResult = (TextView) findViewById(R.id.txtResult);
-
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        btn4.setOnClickListener(this);
-        btn5.setOnClickListener(this);
-        btn6.setOnClickListener(this);
-        btn7.setOnClickListener(this);
-        btn8.setOnClickListener(this);
-        btn9.setOnClickListener(this);
-        btn0.setOnClickListener(this);
-        btnDel.setOnClickListener(this);
-        btnDot.setOnClickListener(this);
-        btnDevide.setOnClickListener(this);
-        btnMulti.setOnClickListener(this);
-        btnSub.setOnClickListener(this);
-        btnSum.setOnClickListener(this);
-        btnResult.setOnClickListener(this);
     }
-
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
@@ -75,58 +85,162 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnNum0:
-                number1 += "0";
+            {
+                actionOfNumbers(0);
                 break;
+            }
             case R.id.btnNum1:
-                number1 += "1";
+            {
+                actionOfNumbers(1);
                 break;
+            }
             case R.id.btnNum2:
-                number1 += "2";
+            {
+                actionOfNumbers(2);
                 break;
+            }
             case R.id.btnNum3:
-                number1 += "3";
+            {
+                actionOfNumbers(3);
                 break;
+            }
             case R.id.btnNum4:
-                number1 += "4";
+            {
+                actionOfNumbers(4);
                 break;
+            }
             case R.id.btnNum5:
-                number1 += "5";
+            {
+                actionOfNumbers(5);
                 break;
+            }
             case R.id.btnNum6:
-                number1 += "6";
+            {
+                actionOfNumbers(6);
                 break;
+            }
             case R.id.btnNum7:
-                number1 += "7";
+            {
+                actionOfNumbers(7);
                 break;
+            }
             case R.id.btnNum8:
-                number1 += "8";
+            {
+                actionOfNumbers(8);
                 break;
+            }
             case R.id.btnNum9:
-                number1 += "9";
+            {
+                actionOfNumbers(9);
                 break;
+            }
             case R.id.btnDevide:
-                number1 += "/";
+            {
+                actionOfOperators("chia");
                 break;
+            }
             case R.id.btnNumMulti:
-                number1 += "x";
+            {
+                actionOfOperators("nhan");
                 break;
+            }
             case R.id.btnSub:
-                number1 += "-";
+            {
+                actionOfOperators("tru");
                 break;
+            }
             case R.id.btnSum:
-                number1 += "+";
+            {
+                actionOfOperators("cong");
                 break;
+            }
             case R.id.btnDot:
-                number1 += ".";
+
                 break;
             case R.id.btnDel:
-                number1 = "";
+            {
+                result = 0;
+                temp =0;
                 txtResult.setText("0");
+                isCalculating=false;
                 return;
+            }
             case R.id.btnResult:
-                number1 += "=";
-                break;
+            {
+                if(isCalculating)
+                {
+                    thucHienTinhToan(pheptinh);
+                    isCalculating=false;
+                    result = 0;
+                    temp =0;
+                }
+            }
+            default:break;
         }
-        txtResult.setText(number1);
+    }
+
+    private void actionOfOperators(String pt)       //sự kiện của các button cộng trừ nhân chia
+    {                                               // truyền vào chuỗi ứng với phép tính
+        if(!isCalculating)
+        {
+            isCalculating=true;             //hiện tại không trong chuỗi tính toán thì chỉ gán phép tính
+            pheptinh=pt;
+        }
+        else
+        {
+            thucHienTinhToan(pheptinh);     //ngược lại thì thực hiện tính xong rồi gán
+            pheptinh=pt;
+        }
+    }
+
+    public void thucHienTinhToan(String pheptinh)   //xác nhận việc tính toán giữa result và giá trị temp. sau đó in ra
+    {
+        if(pheptinh.equals("cong"))
+        {
+            result+= temp;
+            showLenManHinh(result);
+        }
+        else if(pheptinh.equals("tru"))
+        {
+            result-= temp;
+            showLenManHinh(result);
+        }
+        else if(pheptinh.equals("nhan"))
+        {
+            result*= temp;
+            showLenManHinh(result);
+        }
+        else if(pheptinh.equals("chia"))
+            if(temp !=0)
+            {
+                result/= temp;
+                showLenManHinh(result);
+            }
+            else
+            {
+                txtResult.setText("ERROR");         //xử lý chia cho 0 -> lỗi -> reset lại từ đầu
+                result=0;
+                isCalculating=false;
+            }
+        temp=0;                                     //tính toán xong thì reset temp về 0
+    }
+    public void showLenManHinh(int param)
+    {
+        txtResult.setText(String.valueOf(param));
+    }
+    public void actionOfNumbers(int button)         //sự kiện của các button số 0-9, truyền vào số tương ứng
+    {
+        if(isCalculating)
+        {
+            temp = temp*10+button;          //nếu đang trong một chuỗi tính toán, biến result để lưu
+                                            // giá trị trước đó nên ko thể dùng trực tiếp result để tránh mất dữ liệu
+            showLenManHinh(temp);           //vì vậy sử dụng biến temp để cộng dồn chuỗi số mà người dùng nhập
+        }
+        else
+        {
+            result=result*10+button;        //bắt đầu một chuỗi tính toán mới, result đóng vai trò là số đầu tiên
+            showLenManHinh(result);         //nếu dùng temp như trên, khi click vào +-x/ sau đó nhập số thì số vừa nhập sẽ đc lưu trên temp
+                                            //thì giá trị temp ban đầu sẽ bị thay đổi do đó tính toán sẽ trả về giá trị sai
+        }
     }
 }
